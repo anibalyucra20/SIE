5b25dd61e2be60a0d0a53d8eb675f7b2e2800b9c
Marant01 <mantgc@gmail.com>
2023-10-04 21:19:07 -0500

    Update estudiantes.php

--- docente/estudiantes.php
+++ docente/estudiantes.php
@@ -132,9 +132,20 @@
                                     <div class="form-group">
                                       <label class="control-label col-md-3 col-sm-3 col-xs-12">Género :
                                       </label>
-                                      <div class="col-md-9 col-sm-9 col-xs-12">
-                                        <input type="date" class="form-control" name="fecha_fin" required="required">
+                                      <div class="col-md-3 col-sm-3 col-xs-6">
+                                      <select class="form-control" name="genero" id="genero" required>
+                                          <option value=""></option>
+                                          <option value="2021">Masculio</option>
+                                          <option value="2022">Femenino</option> 
+                                        </select>
                                         <br><br>
+                                      </div>
+                                    </div>
+                                    <div class="form-group">
+                                      <label class="control-label col-md-3 col-sm-3 col-xs-12">FOTO : </label>
+                                      <div class="col-md-3 col-sm-3 col-xs-9">
+                                        <input type="file" class="form-control" name="fecha_fin" required="required">
+                                        <br>
                                       </div>
                                     </div>
                                     <div align="center">
