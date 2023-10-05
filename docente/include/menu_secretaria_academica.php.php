a234c5ebb772b2fbdba4e4151b4a91b58898222b
anibalyucra20 <anibalyucrac@gmail.com>
2023-10-04 19:52:34 -0500

    update

--- docente/include/menu_secretaria_academica.php.php
+++ docente/include/menu_secretaria_academica.php.php
@@ -1,103 +1,103 @@
 <div class="navbar nav_title" style="border: 0;">
-                        <a href="index.html" class="site_title"><i class="fa fa-pencil"></i> <span>SIE</span></a>
-                    </div>
+  <a href="index.html" class="site_title"><i class="fa fa-pencil"></i> <span>SIE</span></a>
+</div>
 
-                    <div class="clearfix"></div>
+<div class="clearfix"></div>
 <!-- menu profile quick info -->
 <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Bienvenido,</span>
-                <h2>Nombre Perfil</h2>
-              </div>
-            </div>
-            <!-- /menu profile quick info -->
+  <div class="profile_pic">
+    <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+  </div>
+  <div class="profile_info">
+    <span>Bienvenido,</span>
+    <h2>Nombre Perfil</h2>
+  </div>
+</div>
+<!-- /menu profile quick info -->
 
-            <br />
+<br />
 
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section">
-                <h3>General</h3>
-                <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Inicio </a>
-                    
-                  </li>
-                  <li><a><i class="fa fa-edit"></i> Planificación <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="periodo_academico.php">Periodo Académico</a></li>
-                      <li><a href="programacion_clases.php">Programación de Clases</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-desktop"></i> Matrícula <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="matriculas.php">Matrículas</a></li>
-                      <li><a href="licencias.php">Licencias</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-table"></i> Docentes <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="docentes.php">Relación de Docentes</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-bar-chart-o"></i> Estudiantes <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="estudiantes.php">Relación de Estudiantes</a></li>
-                      <li><a href="apoderados.php">Relación de Apoderados</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-clone"></i>Evaluación <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="evaluaciones.php">Registro de Evaluación</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-clone"></i>Mantenimiento <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="datos_intitucionales.php">Datos Institucionales</a></li>
-                      <li><a href="sedes.php">Sedes</a></li>
-                      <li><a href="periodo_lectivo.php">Periodo Léctivo</a></li>
-                      <li><a href="niveles.php">Niveles</a></li>
-                      <li><a href="ciclos.php">Ciclos</a></li>
-                      <li><a href="grados.php">Grados</a></li>
-                      <li><a href="turnos.php">Turnos</a></li>
-                      <li><a href="secciones.php">Secciones</a></li>
-                      <li><a href="cursos.php">Cursos</a></li>
-                      <li><a href="modalidad.php">Modalidades</a></li>
-                      <li><a href="competencias.php">Competencias</a></li>
-                      <li><a href="capacidades.php">Capacidades</a></li>
-                      <li><a href="datos_sistema.php">Datos de Sistema</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <!-- /sidebar menu -->
-          </div>
-        </div>
+<!-- sidebar menu -->
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+  <div class="menu_section">
+    <h3>General</h3>
+    <ul class="nav side-menu">
+      <li><a><i class="fa fa-home"></i> Inicio </a>
 
-        <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-            <nav>
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
+      </li>
+      <li><a><i class="fa fa-edit"></i> Planificación <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="periodo_academico.php">Periodo Académico</a></li>
+          <li><a href="programacion_clases.php">Programación de Clases</a></li>
+        </ul>
+      </li>
+      <li><a><i class="fa fa-desktop"></i> Matrícula <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="matriculas.php">Matrículas</a></li>
+          <li><a href="licencias.php">Licencias</a></li>
+        </ul>
+      </li>
+      <li><a><i class="fa fa-table"></i> Docentes <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="docentes.php">Relación de Docentes</a></li>
+        </ul>
+      </li>
+      <li><a><i class="fa fa-bar-chart-o"></i> Estudiantes <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="estudiantes.php">Relación de Estudiantes</a></li>
+          <li><a href="apoderados.php">Relación de Apoderados</a></li>
+        </ul>
+      </li>
+      <li><a><i class="fa fa-clone"></i>Evaluación <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="evaluaciones.php">Registro de Evaluación</a></li>
+        </ul>
+      </li>
+      <li><a><i class="fa fa-clone"></i>Mantenimiento <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="datos_intitucionales.php">Datos Institucionales</a></li>
+          <li><a href="sedes.php">Sedes</a></li>
+          <li><a href="periodo_lectivo.php">Periodo Léctivo</a></li>
+          <li><a href="niveles.php">Niveles</a></li>
+          <li><a href="ciclos.php">Ciclos</a></li>
+          <li><a href="grados.php">Grados</a></li>
+          <li><a href="turnos.php">Turnos</a></li>
+          <li><a href="secciones.php">Secciones</a></li>
+          <li><a href="cursos.php">Cursos</a></li>
+          <li><a href="modalidad.php">Modalidades</a></li>
+          <li><a href="competencias.php">Competencias</a></li>
+          <li><a href="capacidades.php">Capacidades</a></li>
+          <li><a href="datos_sistema.php">Datos de Sistema</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+<!-- /sidebar menu -->
+</div>
+</div>
 
-              <ul class="nav navbar-nav navbar-right">
-                <li class="">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">Nombre
-                    <span class=" fa fa-angle-down"></span>
-                  </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Mi perfil</a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesión </a></li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <!-- /top navigation -->
+<!-- top navigation -->
+<div class="top_nav">
+  <div class="nav_menu">
+    <nav>
+      <div class="nav toggle">
+        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+      </div>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li class="">
+          <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+            <img src="images/img.jpg" alt="">Nombre
+            <span class=" fa fa-angle-down"></span>
+          </a>
+          <ul class="dropdown-menu dropdown-usermenu pull-right">
+            <li><a href="javascript:;"> Mi perfil</a></li>
+            <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesión </a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<!-- /top navigation -->
---
+++ docente/registrar_matricula.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Registrar Matrícula | SIE</title>
+    <!--icono en el titulo-->
+    <link rel="shortcut icon" href="">
+
+    <!-- Bootstrap -->
+    <link href="../plantilla/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../plantilla/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../plantilla/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../plantilla/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- Datatables -->
+    <link href="../plantilla/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="../plantilla/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="../plantilla/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="../plantilla/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="../plantilla/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../plantilla/build/css/custom.min.css" rel="stylesheet">
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+
+
+                    <?php include("include/menu_secretaria_academica.php.php"); ?>
+
+                    <!-- page content -->
+                    <div class="right_col" role="main">
+                        <div class="">
+                            <div class="page-title">
+                                <div class="title_left">
+
+                                </div>
+
+
+                            </div>
+                            <div class="clearfix"></div>
+                            <h2>Registrar Matrícula</h2>
+                            <div class="row">
+
+                                <div class="col-md-6 col-sm-12 col-xs-12">
+                                    <div class="x_panel">
+                                        <div class="x_title">
+                                            <h2>Datos de Matrícula</h2>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div class="x_content">
+                                            <br />
+                                            <div class="x_content">
+                                                <form action="" class="form-horizontal form-label-left input_mask">
+                                                    <div class="form-group">
+                                                        <label class="control-label col-md-4 col-sm-4 col-xs-12">DNI:</label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12">
+                                                            <input type="number" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="control-label col-md-4 col-sm-4 col-xs-12">Apellidos y Nombres:</label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12">
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="control-label col-md-4 col-sm-4 col-xs-12">Nivel Académico:</label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12">
+                                                            <select name="" id="" class="form-control">
+                                                                <option value=""></option>
+                                                                <option value="">Secundaria</option>
+                                                                <option value="">Primaria</option>
+                                                                <option value="">Inicial</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="control-label col-md-4 col-sm-4 col-xs-12">Grado Académico:</label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12">
+                                                            <select name="" id="" class="form-control">
+                                                                <option value=""></option>
+                                                                <option value="">1ro</option>
+                                                                <option value="">2do</option>
+                                                                <option value="">3ro</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="control-label col-md-4 col-sm-4 col-xs-12">Turno:</label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12">
+                                                            <select name="" id="" class="form-control">
+                                                                <option value=""></option>
+                                                                <option value="">Mañana</option>
+                                                                <option value="">Tarde</option>
+                                                                <option value="">Noche</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="control-label col-md-4 col-sm-4 col-xs-12">Sección:</label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12">
+                                                            <select name="" id="" class="form-control">
+                                                                <option value=""></option>
+                                                                <option value="">A</option>
+                                                                <option value="">B</option>
+                                                                <option value="">C</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input type="hidden" id="arr_uds" name="arr_uds">
+                                                        <input type="hidden" id="mat_relacion" name="mat_relacion" required>
+                                                        <label class="col-md-4 col-sm-4 col-xs-12 control-label">Seleccione las Asignaturas :
+                                                        </label>
+                                                        <div class="col-md-8 col-sm-8 col-xs-12" id="udss">
+                                                            <div class="checkbox">
+                                                                <label>
+                                                                    <input type="checkbox" id="all_check"><b> SELECCIONAR TODAS LAS ASIGNATURAS *</b>
+                                                                </label>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-12 col-xs-12">
+                                    <div class="x_panel">
+                                        <div class="x_title">
+                                            <h2>Asignaturas a Matrícular</h2>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div class="x_content">
+                                            <br />
+                                            <div class="x_content">
+                                                <div class="form-group">
+                                                    <label class="col-md-3 col-sm-3 col-xs-12 control-label">
+                                                    </label>
+                                                    <div class="col-md-9 col-sm-9 col-xs-12" id="uds_selec">
+                                                        Aún no hay Asignaturas Agregadas para la Matrícula
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /page content -->
+
+                    <!-- footer content -->
+                    <footer>
+                        <div class="pull-right">
+                            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </footer>
+                    <!-- /footer content -->
+                </div>
+            </div>
+
+            <!-- jQuery -->
+            <script src="../plantilla/vendors/jquery/dist/jquery.min.js"></script>
+            <!-- Bootstrap -->
+            <script src="../plantilla/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+            <!-- FastClick -->
+            <script src="../plantilla/vendors/fastclick/lib/fastclick.js"></script>
+            <!-- NProgress -->
+            <script src="../plantilla/vendors/nprogress/nprogress.js"></script>
+            <!-- iCheck -->
+            <script src="../plantilla/vendors/iCheck/icheck.min.js"></script>
+            <!-- Datatables -->
+            <script src="../plantilla/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+            <script src="../plantilla/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+            <script src="../plantilla/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+            <script src="../plantilla/vendors/jszip/dist/jszip.min.js"></script>
+            <script src="../plantilla/vendors/pdfmake/build/pdfmake.min.js"></script>
+            <script src="../plantilla/vendors/pdfmake/build/vfs_fonts.js"></script>
+
+            <!-- Custom Theme Scripts -->
+            <script src="../plantilla/build/js/custom.min.js"></script>
+
+            <script>
+                $(document).ready(function() {
+                    $('#example').DataTable({
+                        "language": {
+                            "processing": "Procesando...",
+                            "lengthMenu": "Mostrar _MENU_ registros",
+                            "zeroRecords": "No se encontraron resultados",
+                            "emptyTable": "Ningún dato disponible en esta tabla",
+                            "sInfo": "Mostrando del _START_ al _END_ de un total de _TOTAL_ registros",
+                            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                            "search": "Buscar:",
+                            "infoThousands": ",",
+                            "loadingRecords": "Cargando...",
+                            "paginate": {
+                                "first": "Primero",
+                                "last": "Último",
+                                "next": "Siguiente",
+                                "previous": "Anterior"
+                            },
+                        }
+                    });
+
+                });
+            </script>
+
+</body>
+
+</html>
